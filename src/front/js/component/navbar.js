50bd3d3a2ff82802002da7591617f6450a825a84
Sharguidev <guillermoj.obando@gmail.com>
2024-12-20 16:04:31 +0000

    Navbar editado, imagen de navbar añadida y boton descargar CV

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,17 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { DownloadCV } from "./downloadCV";
+import "./../../styles/navbar.css";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className="navbar navbar-expand-lg bg-body-tertiary">
 			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+				<Link to="/" className="navbar-brand navd-link" >Sharguidev Portafolio</Link>
+				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+					<div className="navbar-nav ms-auto gap-2 navd-link">
+						<Link to="/" className="nav-link active navd-link" aria-current="page">About Me</Link>
+						<Link to="/" className="nav-link navd-link" >Projects</Link>
+						<DownloadCV />
+						{/* <Link className="nav-link" to="/"></Link> */}
+						<Link to="/" className="nav-link navd-link" >Skills</Link>
+						<Link to="/" className="nav-link navd-link" >Contact Me</Link>
+					</div>
 				</div>
 			</div>
 		</nav>
